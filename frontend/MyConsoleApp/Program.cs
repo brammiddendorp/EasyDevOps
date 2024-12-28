336fd98ecdcdea3b1e1bcd83005815fbadb4f3f8
Bram Middendorp <71711096+brammiddendorp@users.noreply.github.com>
2024-12-28 19:16:02 +0100

    Add basic frontend

---
+++ frontend/MyConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// Print title
+Console.WriteLine("EasyDevOps");
+
+// Print logo
+Console.WriteLine(@"
+ ___ _____ __  __ 
+|_ _|_   _|  \/  |
+ | |  | | | |\/| |
+ | |  | | | |  | |
+|___| |_| |_|  |_|
+");
